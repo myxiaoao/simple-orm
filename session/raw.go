7783641ac885ba216575ac8642ec527d7463c652
Cooper <myxiaoao@gmail.com>
2022-05-17 11:38:17 +0800

    对象表结构映射

--- session/raw.go
+++ session/raw.go
@@ -2,34 +2,39 @@ package session
 
 import (
 	"database/sql"
+	"simple-orm/dialect"
 	"simple-orm/log"
+	"simple-orm/schema"
 	"strings"
 )
 
+// Session keep a pointer to sql.DB and provides all execution of all
+// kind of database operations.
 type Session struct {
-	db      *sql.DB
-	sql     strings.Builder
-	sqlVars []interface{}
+	db       *sql.DB
+	dialect  dialect.Dialect
+	refTable *schema.Schema
+	sql      strings.Builder
+	sqlVars  []interface{}
 }
 
-func New(db *sql.DB) *Session {
-	return &Session{db: db}
+// New creates instance of Session
+func New(db *sql.DB, dialect dialect.Dialect) *Session {
+	return &Session{
+		db:      db,
+		dialect: dialect,
+	}
 }
 
+// Clear initialize the state of a session
 func (s *Session) Clear() {
 	s.sql.Reset()
 	s.sqlVars = nil
 }
 
+// DB returns *sql.DB
 func (s *Session) DB() *sql.DB {
 	return s.db
-}
-
-func (s *Session) Raw(sql string, values ...interface{}) *Session {
-	s.sql.WriteString(sql)
-	s.sql.WriteString(" ")
-	s.sqlVars = append(s.sqlVars, values...)
-	return s
 }
 
 // Exec raw sql with sqlVars
@@ -57,4 +62,12 @@ func (s *Session) QueryRows() (rows *sql.Rows, err error) {
 		log.Error(err)
 	}
 	return
+}
+
+// Raw appends sql and sqlVars
+func (s *Session) Raw(sql string, values ...interface{}) *Session {
+	s.sql.WriteString(sql)
+	s.sql.WriteString(" ")
+	s.sqlVars = append(s.sqlVars, values...)
+	return s
 }
